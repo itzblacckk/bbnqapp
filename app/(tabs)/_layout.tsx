175644b4877ba67b1aa93f10cc77ca1499235545
sahaskamble <sahaskamble282@gmail.com>
2024-05-22 22:47:54 +0530

    half navbar completed

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { View } from 'react-native';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -13,14 +14,26 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarShowLabel: false,
+        tabBarShowLabel: false, 
+        tabBarStyle:{
+          height: 60,
+          borderRadius: 20,
+          margin: 10,
+          shadowOffset: { width: 0, height: 5 },
+          shadowColor: '#000',
+          shadowRadius: 10,
+          shadowOpacity: 30,
+        } 
       }}>
       <Tabs.Screen
         name="home"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={'#5de65d'} />
+          tabBarIcon: ({ focused }) => (
+            // <TabBarIcon name={focused ? 'home' : 'home-outline'} color={'#5de65d'} />
+            <View style={ focused ? { backgroundColor: 'red', width: 50, height: 50, borderRadius: 100, position: 'relative', top: -25, display: 'flex', alignSelf: 'center', alignItems: 'center', justifyContent: 'center' } : { backgroundColor: 'red', width: 50, height: 50, borderRadius: 100, display: 'flex', alignSelf: 'center', alignItems: 'center', justifyContent: 'center' }}>
+              <TabBarIcon name={'home'} color={'#5de65d'} size={35} style={{ marginBottom: 5 }}  />
+            </View>
           ),
         }}
       />
@@ -28,7 +41,7 @@ export default function TabLayout() {
         name="explore"
         options={{
           title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
+          tabBarIcon: ({ focused }) => (
             <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={'#5de65d'} />
           ),
         }}
@@ -37,7 +50,7 @@ export default function TabLayout() {
         name="profile"
         options={{
           title: 'Profile',
-          tabBarIcon: ({ color, focused }) => (
+          tabBarIcon: ({ focused }) => (
             <TabBarIcon name={focused ? 'people-sharp' : 'people-outline' } color={'#5de65d'} />
           ),
         }}
